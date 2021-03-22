6d2834ce34df1b4614049ff022634317d2e274d0
abaxsoraszem <abax.soraszem@gmail.com>
2021-03-22 10:03:00 +0100

    create non existing required directory

--- ringFace/ringUtils/dirStructure.py
+++ ringFace/ringUtils/dirStructure.py
@@ -1,24 +1,16 @@
 from decouple import config
+from pathlib import Path
+
 
 DATA_DIR = config('DATA_DIR')
 
-
-# class DirStructure():
-#     def __init__(self, dataDir = DATA_DIR, imagesDir = IMAGES_DIR, recogniserDir = RECOGNISER_DIR, classifierDir = CLASSIFIER_DIR):
-#         self.dataDir = dataDir
-#         self.imagesDir = imagesDir
-#         self.recogniserDir = recogniserDir
-#         self.classifierDir = classifierDir
-
-#         self.unprocessedEvents = dataDir + "/events/unprocessed"
-#         self.processedEvents = dataDir + "/events/processed"
-
-#         self.images = dataDir + "/images"
 
 class DirStructure():
     def __init__(self, dataDir = DATA_DIR):
         self.images = dataDir + "/images"
         self.classifierDir = dataDir + "/classifier"
+        Path(self.images).mkdir(parents=True, exist_ok=True)
+        Path(self.classifierDir).mkdir(parents=True, exist_ok=True)
 
 
 DEFAULT_DIR_STUCTURE = DirStructure()
