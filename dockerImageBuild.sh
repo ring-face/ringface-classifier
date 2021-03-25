0378bbc361cbfe0d5a475c71ebf785cf4537356c
abaxsoraszem <abax.soraszem@gmail.com>
2021-03-25 09:09:33 +0100

    configurable docker builders

--- dockerImageBuild.sh
+++ dockerImageBuild.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
+cd "$(dirname "$0")"
+
 docker build . --tag ringface/classifier:latest
+
+if [ -z ${1+x} ]
+  then
+  echo "semver is not defined"
+  else
+  echo "tagging to '$1'"
+  docker tag ringface/classifier:latest ringface/classifier:$1
+fi
